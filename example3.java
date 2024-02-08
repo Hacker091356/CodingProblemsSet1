d8372e84adcfe0d281f5c69f95ed4cc006e07236
Hacker091356 <ninno2812@gmail.com>
2024-02-08 14:54:30 -0500

    example3

---
+++ example3.java
@@ -0,0 +1,12 @@
+/******
+ * Project: Example 1 
+ * Programmer: Neil Oza
+ * Date:  2/8/24
+ ****************/
+
+ public class example3{
+     public static void main(String[] args){
+         System.out.println("\t\t\t\t\t Neil Oza \n\n Song: Swinging like Ali knocking out bodies \n Poem: If you're right keep your mouth shut! \n Bumper Sticker: I'm not speeding I'm qualifying\n");
+     }
+ }
+ 
