c6f500f8e968f81b42cb405076abe31cf2ea9889
Hacker091356 <ninno2812@gmail.com>
2024-02-08 14:43:55 -0500

    Example2

--- example2.java
+++ example2.java
@@ -1,3 +1,12 @@
-public class example2 {
-    
-}
+/******
+ * Project: Example 1 
+ * Programmer: Neil Oza
+ * Date:  2/8/24
+ ****************/
+
+ public class example2{
+
+    public static void main (String[] args){
+        System.out.println("****************\n*Sue's Swimwear*\n****************");
+    }
+ }
