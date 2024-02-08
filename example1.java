352fcb465121fffc5de3ddf8968b1b073c685a23
Hacker091356 <ninno2812@gmail.com>
2024-02-08 14:37:50 -0500

    example1

--- example1.java
+++ example1.java
@@ -6,6 +6,7 @@
 
 public class example1 {
 
-    public static void main (String[] args)
+    public static void main (String[] args){
+        System.out.println("\n\n\n\n\n\t\t\t\t TZHS" );
+    }
 }
-System.out.println("\n\n\n\n\n\t\t\t\t TZHS" )
